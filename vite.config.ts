50f6ecbaa93930f3d4752e70ac1156a6e736d75d
Alroy Dsouza <dalroy44@gmail.com>
2025-07-23 15:06:18 +0530

    Add Codecov bundler plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { codecovVitePlugin } from '@codecov/vite-plugin'
 
 export default defineConfig({
   // 👇 ADD THIS 'base' PROPERTY 👇
   base: '/global-ecommerce-dashboard/', // e.g., '/my-awesome-dashboard/'
-  plugins: [react()],
+  plugins: [
+    react(),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: '<bundle project name>',
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
